88b16b18c08c967b920dc9fc0224bcc0bdd51d0c
unkarjedy <unkarjedy@mail.ru>
2015-07-01 15:31:39 +0430

    initial commit

---
+++ desktop/src/com/unkarjedy/platformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.unkarjedy.platformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.unkarjedy.platformer.PlatformerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlatformerGame(), config);
+	}
+}
